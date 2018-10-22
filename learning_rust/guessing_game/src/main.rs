aa08502de95b5145b213cab4ee182ad4395f75d4
mfekadu <mike.fekadu@gmail.com>
2018-10-22 16:47:33 -0700

    added a loop and error handling - program complete

--- learning_rust/guessing_game/src/main.rs
+++ learning_rust/guessing_game/src/main.rs
@@ -11,57 +11,74 @@ fn main() {
 
   let secret_number = rand::thread_rng().gen_range(1,101);
 
-  println!("The secret number is: {}", secret_number);
+  /* it is not a secret if you tell anyone ;) */
+  // println!("The secret number is: {}", secret_number);
 
-  println!("Please input your guess.");
+  /* like while(true) loop */
+  loop {
 
-  /* define a new, mutable, empty string */
-  /* the "::" means "new" is "associated" with "String"
-   * because "String" is a "type" 
-   * rather than a "particular instance of a type"
-   */
-  let mut guess = String::new();
+    println!("Please input your guess.");
 
-  /* again "stdin" is associated function of "io" */
-  /* if not `use std::io` then need say `std::io::stdin` */
-  /* stdin() returns "instance" of `std:io::Stdin` type */
-  /* that instance has the read_line() function
-   *    which takes in a pointer to a mutable String buffer 
-   *    and it will fill the bufer with the user input
-   *    but `&guess` would be an IMMUTABLE pointer
-   *    so `&mut guess` gives you a MUTABLE pointer
-   */
-  io::stdin().read_line(&mut guess)
-    .expect("Failed to read line");
-  /* lastly the error handling:
-   *    read_line() retuns a type "std::io::Result"
-   *    "io::Result" types are enums with Ok or Err
-   *    expect("str") 
-   *     will safely unwrap an Ok and panic with "str" if Err
-   */
+    /* define a new, mutable, empty string */
+    /* the "::" means "new" is "associated" with "String"
+     * because "String" is a "type" 
+     * rather than a "particular instance of a type"
+     */
+    let mut guess = String::new();
 
-  /* print with string format */
-  println!("You guessed: {}", guess);
+    /* again "stdin" is associated function of "io" */
+    /* if not `use std::io` then need say `std::io::stdin` */
+    /* stdin() returns "instance" of `std:io::Stdin` type */
+    /* that instance has the read_line() function
+     *    which takes in a pointer to a mutable String buffer 
+     *    and it will fill the bufer with the user input
+     *    but `&guess` would be an IMMUTABLE pointer
+     *    so `&mut guess` gives you a MUTABLE pointer
+     */
+    io::stdin().read_line(&mut guess)
+      .expect("Failed to read line");
+    /* lastly the error handling:
+     *    read_line() retuns a type "std::io::Result"
+     *    "io::Result" types are enums with Ok or Err
+     *    expect("str") 
+     *     will safely unwrap an Ok and panic with "str" if Err
+     */
 
-  /* convert guess to a 32 bit unsigned int */
-  /* Note: Rust is strongly and statically typed */
-  /* trim() will eliminate the '\n'
-   * parse() will convert to some kind of number
-   * the ':' after guess tells Rust to annotate the variable type
-   * expect() will handle if say "A*$" was the guess
-   */
-  let guess: u32 = guess.trim().parse()
-    .expect("Please type a number!");
+    /* print with string format */
+    println!("You guessed: {}", guess);
 
-  /* soo..I suppose you can do .cmp() on any "std" type? or subtype?? */
-  /* "match" is a control-flow operator, kinda like switch-case, but not
-   *   "a match expression is made of 'arms'"
-   *   "an arm consists of a 'pattern'"
-   *   "Rust takes the value given to match and looks through each pattern"
-   */
-  match guess.cmp(&secret_number) {
-    Ordering::Less => println!("Too small!"),   /* guess <  secret */
-    Ordering::Greater => println!("Too big!"),  /* guess >  secret */
-    Ordering::Equal => println!("You got it!"), /* guess == secret */
+    /* convert guess to a 32 bit unsigned int */
+    /* NOTE: Rust is strongly and statically typed */
+    /* trim() will eliminate the '\n'
+     * parse() will convert to some kind of number
+     * the ':' after guess tells Rust to annotate the variable type
+     * expect() will handle if say "A*$" was the guess
+     *    update....
+     *    instead of that, handle each variant of io::Result
+     *    because expect() will just crash with given message and backtrace
+     */
+    let guess: u32 = match guess.trim().parse() {
+      /* NOTE: this is a match-expression for error-handling */
+      Ok(num) => num,     /* put the Ok(result) into `guess` variable */
+      Err(_) => { 
+        println!("Please input a number");
+        continue /* next iteration of loop */
+      }
+    };
+
+    /* soo..I suppose you can do .cmp() on any "std" type? or subtype?? */
+    /* "match" is a control-flow operator, kinda like switch-case, but not
+     *   "a match expression is made of 'arms'"
+     *   "an arm consists of a 'pattern'"
+     *   "Rust takes the value given to match and looks through each pattern"
+     */
+    match guess.cmp(&secret_number) {
+      Ordering::Less => println!("Too small!"),  /* guess <  secret */
+      Ordering::Greater => println!("Too big!"), /* guess >  secret */
+      Ordering::Equal => {                       /* guess == secret */
+        println!("You got it!");
+        break; /* break out of loop */
+      } 
+    }
   }
 }
