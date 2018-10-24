a739c2526e0ea15d253d8745fe4560beb1bcec98
mfekadu <mike.fekadu@gmail.com>
2018-10-23 21:57:53 -0700

    some explanations

--- algorithms/greedy_stocks/src/main.rs
+++ algorithms/greedy_stocks/src/main.rs
@@ -8,10 +8,13 @@
 
 
 
-/* the function "getMaxProfit" 
- * takes input "stockPrices" which is an immutable pointer to a i32
+/* the function "get_max_profit" 
+ * takes input "stock_prices" which is an immutable pointer to a i32
  *    https://stackoverflow.com/a/24833065/5411712
  *    I feel like this is the most basic thing, but still so hard...
+ * takes input "num_stocks" which is a usize
+ *    for the length of the stock_prices array. 
+ * outputs an i32 for the maximum profit after buying low and selling high.
  */
 fn get_max_profit(stock_prices: &[i32], num_stocks: usize) -> i32 { 
   stock_prices[num_stocks - 1]
@@ -20,9 +23,12 @@ fn get_max_profit(stock_prices: &[i32], num_stocks: usize) -> i32 {
 
 fn main() {
   const NUM_STOCKS: usize = 6;
+
   /* this defines a fixed length immutable array of i32 values */ 
   let stock_prices: [i32; NUM_STOCKS] = [10, 7, 5, 8, 11, 9];
-  println!("Hello, world!");
-  println!("stock_prices: {:?}", stock_prices);
-  println!("max_profit: {}", get_max_profit(&stock_prices, NUM_STOCKS));
+
+  println!("stock_prices = {:?}", stock_prices);
+
+  println!("max_profit = {}", get_max_profit(&stock_prices, NUM_STOCKS));
+  // expect max_profit = 6 (buying for $5 and selling for $11)
 }
