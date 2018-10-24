ff277d4ecf238e6dac16521b67f7181c66d99481
mfekadu <mike.fekadu@gmail.com>
2018-10-23 22:32:34 -0700

    sure learned a lot here
    special thanks to these links:
    
    https://www.interviewcake.com/question/c/stock-price?section=greedy&course=fc1
    
    https://github.com/rust-lang/rfcs/issues/1362
    
    https://doc.rust-lang.org/std/cmp/fn.max.html
    
    https://doc.rust-lang.org/book/second-edition/ch15-02-deref.html
    
    https://doc.rust-lang.org/book/2018-edition/ch03-05-control-flow.html
    
    https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet#links

--- algorithms/greedy_stocks/src/main.rs
+++ algorithms/greedy_stocks/src/main.rs
@@ -6,7 +6,7 @@
  *    - "->" is used to define the return type
  */
 
-
+/* https://www.interviewcake.com/question/c/stock-price?section=greedy&course=fc1 */
 
 /* the function "get_max_profit" 
  * takes input "stock_prices" which is an immutable pointer to a i32
@@ -17,7 +17,44 @@
  * outputs an i32 for the maximum profit after buying low and selling high.
  */
 fn get_max_profit(stock_prices: &[i32], num_stocks: usize) -> i32 { 
-  stock_prices[num_stocks - 1]
+  let mut min_price:i32;
+  let mut max_profit:i32;
+
+  /* make sure we have at least 2 stocks, using the assert! macro */
+  assert!(num_stocks >= 2);
+
+  /* goal is to greedily update min_price and max_profit
+   * so initialize with first possible values
+   */
+  min_price = stock_prices[0];
+  max_profit = stock_prices[1] - stock_prices[0];
+
+  /* start at the second index
+   * selling at first index is illegal, because buying must happen first
+   *    and beacuase buying and selling at the same time is illegal
+   * problem statement asks for negative i32 value if array is decreasing.
+   */
+  /* Rust control flow:
+   * https://doc.rust-lang.org/book/2018-edition/ch03-05-control-flow.html
+   */ 
+  for t in 1..num_stocks { /* from t=1 to t=num_stocks */
+    let current_price:i32 = stock_prices[t];
+
+    let potential_profit = current_price - min_price;
+
+    /* found a lengthy debate on the lack of ternary operator in Rust 
+     *    https://github.com/rust-lang/rfcs/issues/1362
+     */
+
+    /* update max to try to do better */
+    /* I purposely did not have `use std::cmp;` for funsies */
+    max_profit = std::cmp::max(max_profit, potential_profit);
+
+    /* update min, to always have the smallest so far */
+    min_price = std::cmp::min(min_price, current_price);
+  } 
+
+  max_profit /* optionally say return max_profit; */
 }
 
 
