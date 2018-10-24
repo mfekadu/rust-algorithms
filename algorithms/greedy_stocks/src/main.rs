66ba46bba8e99f09500e226ddfd38aff1c476d47
mfekadu <mike.fekadu@gmail.com>
2018-10-23 21:36:34 -0700

    and this is what it takes to print an array

--- algorithms/greedy_stocks/src/main.rs
+++ algorithms/greedy_stocks/src/main.rs
@@ -13,12 +13,15 @@
  *    https://stackoverflow.com/a/24833065/5411712
  *    I feel like this is the most basic thing, but still so hard...
  */
-fn get_max_profit(stock_prices: &mut [i32]) -> i32 { 
-  stock_prices[0] /* dereference the pointer at 0??? */
+fn get_max_profit(stock_prices: &mut [i32; 6]) -> [i32; 6] { 
+  *stock_prices /* dereference the pointer at 0??? */
 }
 
+
 fn main() {
-  let mut stock_prices = [10, 7, 5, 8, 11, 9];
+  const LENGTH:usize = 6;
+  /* this defines a mutable array LENGTH number of i32 values */ 
+  let mut stock_prices: [i32; LENGTH] = [10, 7, 5, 8, 11, 9];
   println!("Hello, world!");
-  println!("{}", get_max_profit(&mut stock_prices));
+  println!("{:?}", get_max_profit(&mut stock_prices));
 }
